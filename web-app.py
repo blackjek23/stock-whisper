6405e9a557b9a4708cac0568ecf4eec97fd98a60
yakov shachar <yakov.shahar97@gmail.com>
2024-09-19 11:48:48 +0300

    modify web part to make it more orgenized and adding template for future strategies

--- web-app.py
+++ web-app.py
@@ -2,14 +2,44 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the home page to list available strategies
+@app.route('/')
+def home():
+    strategies = [
+        {"name": "EMA150 Strategy", "url": "/ema150"},
+        {"name": "Bollinger Bands Strategy", "url": "/bband"},
+        # {"name": "New Strategy", "url": "/new_strategy"}  # Add your new strategy here
+    ]
+    return render_template('home.html', strategies=strategies)
+
+# Route for the EMA150 strategy
 @app.route('/ema150')
-def index():
-    # Read the contents of the text file
+def ema150():
+    # Read the contents of the EMA150 text file
     with open('./Alerts/ema150.txt', 'r') as file:
         content = file.read()
 
     # Pass the content to the HTML template
-    return render_template('index.html', content=content)
+    return render_template('strategy.html', strategy_name="EMA150 Strategy", content=content)
+
+# Route for the Bollinger Bands strategy
+@app.route('/bband')
+def bband():
+    # Read the contents of the Bollinger Bands text file
+    with open('./Alerts/Bband.txt', 'r') as file:
+        content = file.read()
+
+    # Pass the content to the HTML template
+    return render_template('strategy.html', strategy_name="Bollinger Bands Strategy", content=content)
+
+# @app.route('/new_strategy')
+# def new_strategy():
+#     # Read the contents of the new strategy's text file
+#     with open('./Alerts/new_strategy.txt', 'r') as file:
+#         content = file.read()
+
+#     # Pass the content to the HTML template
+#     return render_template('strategy.html', strategy_name="New Strategy", content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
